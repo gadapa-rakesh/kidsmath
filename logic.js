91db727eeb76c7e10bd5b3336bf9e1dae6922dc8
Rakesh <gadapa.rakesh@gmail.com>
2022-11-28 21:01:54 -0600

    more complex problem

--- logic.js
+++ logic.js
@@ -8,10 +8,11 @@ function start() {
     nextProblem();
 }
 
+//do nothing
 function updateProblemCount(){
-    numProblems++;
-    var element = document.getElementById(NUM_PROBLEMS);
-    element.innerText = numProblems;
+    // numProblems++;
+    // var element = document.getElementById(NUM_PROBLEMS);
+    // element.innerText = numProblems;
 }
 
 function updateText(id, newVal) {
@@ -23,31 +24,28 @@ function getValue(id) {
     return document.getElementById(id)?.innerText;
 }
 
-function getRandomArbitrary() {
-    return getRandom(0, 10);
-}
-
 function getRandom(min, max) {
     return Math.round(Math.random() * (max - min) + min);
 }
 
 function nextProblem() {
     updateProblemCount();
-    var part1Random = getRandomArbitrary();
+    var part1Random = getRandom(0,20);
     updateText(PART_1, part1Random);
 
-    var part2Random = getRandomArbitrary();
+    var part2Random = getRandom(0,10);
     updateText(PART_2, part2Random);
 
     // var randOp = getRandomOperator();
+    // if(parseInt(part1Random) < parseInt(part2Random)) nextProblem();
     // updateText('operator', randOp);
 }
 
 function getRandomOperator() {
-    var randomOpRef = getRandom(1, 3);
+    var randomOpRef = getRandom(1, 2);
     if(randomOpRef == 1) return '+';
-    else if(randomOpRef === 2) return '-';
-    else return 'x';
+    // else if(randomOpRef === 2) return '-';
+    else return '-';
 }
 
 start();
